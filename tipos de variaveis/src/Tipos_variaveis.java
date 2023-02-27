29ffcd923de07612196eefd12e138bee19731979
Diego <diegomarquessoare@gmail.com>
2023-02-27 17:27:37 -0300

    final

--- tipos de variaveis/src/Tipos_variaveis.java
+++ tipos de variaveis/src/Tipos_variaveis.java
@@ -1,5 +1,13 @@
 public class Tipos_variaveis {
     public static void main(String[] args) {
-float salario= 2.5F;
+        boolean a = true;
+        boolean b = false;
+        boolean resultado= a==b?true:false;
+        System.out.println(resultado);
+        if (a || b) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
     }
 }
